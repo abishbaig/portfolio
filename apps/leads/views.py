de88e20448229531690828f03b989f687d666483
abishbaig <baig.abish423@gmail.com>
2026-01-16 00:40:01 +0500

    Implement Contact Form Backend

--- apps/leads/views.py
+++ apps/leads/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import ContactLeads
+
 
 # Create your views here.
 def contactView(request):
-    
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+        ContactLeads.objects.create(name=name, email=email, message=message)
+        return render(request, "leads/success.html")    
     return render(request, "leads/contact.html")
